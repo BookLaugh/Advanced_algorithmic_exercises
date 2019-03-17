b1b32230b55177b3f485e9e2641677a40d648188
Adam <abgans+github@gmail.com>
2019-03-17 22:09:46 +0100

    Poprawienie struktury kodu
    Krótszy kod to kod łatwiejszy w utrzymaniu. Kod bardziej spójny jest łatwiejszy w odczytaniu. Przy okazji - korzystanie z zip(), reversed() pokazuje niejaką znajomość Pythona.

--- roman_numbers.py
+++ roman_numbers.py
@@ -1,22 +1,22 @@
 print('\n\n')
 
-UNITS_SYMBOLS = ['I', 'IV', 'V', 'IX']
-TENS_SYMBOLS = ['X', 'XL', 'L', 'XC']
-HUNDREDS_SYMBOLS = ['C', 'CD', 'D', 'CM']
-THOUSAND = 'M'
+UNITS_SYMBOLS = ['I', 'V', 'X']
+TENS_SYMBOLS = ['X', 'L', 'C']
+HUNDREDS_SYMBOLS = ['C', 'D', 'M']
+THOUSANDS = ['M'] * 9
 
 
 def ask_for_number_and_check_validity():  # zapytanie o numer i sprawdzenie czy input jest taki jak oczekujemy
     while True:
-        number = input('PLease enter a number from 1 to 3999: ')
+        number_str = input('PLease enter a number from 1 to 3999: ')
         try:
-            int(number)
+            number = int(number_str)
         except ValueError:
             continue
-        if int(number) < 1 or int(number) > 3999:
+        if number < 1 or number > 3999:
             continue
         else:
-            return number
+            return number_str
 
 
 number = ask_for_number_and_check_validity()
@@ -24,51 +24,33 @@ number = ask_for_number_and_check_validity()
 
 def change_number_to_reversed_list(number):
     # zmiana numeru na odwrocona liste w celu sprawdzenia cyfr od jednosci
-    number_list = list(number)
-    number_list = [int(i) for i in number_list]
-    number_list.reverse()
-    return number_list
-
+    return [int(i) for i in reversed(number)]
 
 reversed_number_list = change_number_to_reversed_list(number)
 
 
-def check_digit(reversed_number_list, symbols, i, roman_list):  # sprawdzenie pojedynczej cyfry z odwróconej listy i dodanie rzymskiej do nowej listy
-    if 0 < reversed_number_list[i] < 4:
-        for j in range(reversed_number_list[i]):
+def check_digit(reversed_number, symbols, roman_list):  # sprawdzenie pojedynczej cyfry z odwróconej listy i dodanie rzymskiej do nowej listy
+    if 0 < reversed_number < 4:
+        for j in range(reversed_number):
             roman_list.append(symbols[0])
-    elif 5 < reversed_number_list[i] < 9:
-        for j in range(reversed_number_list[i]-5):
+    elif 5 < reversed_number < 9:
+        for j in range(reversed_number - 5):
             roman_list.append(symbols[0])
-        roman_list.append(symbols[2])
-    elif reversed_number_list[i] == 4:
         roman_list.append(symbols[1])
-    elif reversed_number_list[i] == 9:
-        roman_list.append(symbols[3])
-    elif reversed_number_list[i] == 5:
+    elif reversed_number == 4:
+        roman_list.append(symbols[1])
+        roman_list.append(symbols[0])
+    elif reversed_number == 9:
         roman_list.append(symbols[2])
+        roman_list.append(symbols[0])
+    elif reversed_number == 5:
+        roman_list.append(symbols[1])
 
 
 def check_number_digits_and_add_roman_symbols(reversed_number_list, number):  # stworzenie listy z rzymskimi znakami
     roman_list = []
-    for i in range(len(number)):
-        if i == 0:
-            symbols = UNITS_SYMBOLS
-            check_digit(reversed_number_list, symbols, i, roman_list)
-            i += 1
-        elif i == 1:
-            symbols = TENS_SYMBOLS
-            check_digit(reversed_number_list, symbols, i, roman_list)
-            i += 1
-        elif i == 2:
-            symbols = HUNDREDS_SYMBOLS
-            check_digit(reversed_number_list, symbols, i, roman_list)
-            i += 1
-        elif i == 3:
-            thousands = reversed_number_list[i]*THOUSAND
-            roman_list.append(thousands)
-        else:
-            i += 1
+    for symbols, i in zip([UNITS_SYMBOLS, TENS_SYMBOLS, HUNDREDS_SYMBOLS, THOUSANDS], range(len(number))):
+        check_digit(reversed_number_list[i], symbols, roman_list)
     return roman_list
 
 
@@ -76,9 +58,7 @@ roman_list = check_number_digits_and_add_roman_symbols(reversed_number_list, num
 
 
 def change_list_to_roman_number(roman_list):  # odwrócenie listy z rzymskimi znakami i polaczenie elementow w celu stworzenia rzymskiej liczby
-    roman_list.reverse()
-    roman_number = ''.join(roman_list)
-    return roman_number
+    return ''.join(reversed(roman_list))
 
 
 roman_number = change_list_to_roman_number(roman_list)
